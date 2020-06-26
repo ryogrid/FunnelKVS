f729376bdd90696e8a73e0455e5baa14919b953a
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-06-26 17:04:21 +0900

    worked example code using Rocket web framework at referencing qiita article.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rust_dkvs
+
+- Referenced site  
+  - https://qiita.com/yukinarit/items/c5128e67d168b4f39983  
+  - https://rocket.rs/v0.4/guide/getting-started/
+
+- Commands execution needed to use Rocket at project directory
+  - $ rustup override set nightly
+  - $ rustup update && cargo update
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene)]
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+/// GETがきたときに"Hello, world!"というレスポンスを返す
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])  // ここにルーティングをセットする
+        .launch();
 }

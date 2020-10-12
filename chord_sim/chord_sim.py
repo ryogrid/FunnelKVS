05e14bd6b8163798e54d27f6e316adccfefae88e
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-10-12 20:01:06 +0900

    wip

--- chord_sim/chord_sim.py
+++ chord_sim/chord_sim.py
@@ -1,6 +1,44 @@
 # coding:utf-8
 
-class ChordNode:
+# アドレス文字列をキーとしてとり、対応するノードのChordNodeオブジェクトを返すハッシュ
+# IPアドレスが分かれば、対応するノードと通信できることと対応している
+all_node_dict = {}
 
-    def ChordNode(self):
+class NodeInfo:
+    id_str = None
+    address_str = None
+    # 半閉区間 (start, end] で startの値は含まない
+    assigned_range_start = None
+    assigned_range_end = None
+
+    def __init__(self, **params):
+        # メンバ変数に代入していく
+        for key, val in params.items():
+            if hasattr(self, key):
+                self.__dict__[key] = val
+
+class ChordNode:
+    node_info = None
+    #KeyもValueもどちらも文字列. Keyはハッシュを通されたものなので元データの値とは異なる
+    stored_data = {}
+    predicessor = None
+    finger_table = []
+    successors = []
+
+    def __init__(self):
         print("hello Chord!")
+
+    def put(self, key_str, value_str):
+        print("not implemented yet")
+
+    def get(self, key_str):
+        print("not implemented yet")
+
+    def delete(self, key_str):
+        print("not implemented yet")
+
+def main():
+    print("not implemented yet")
+
+if __name__ == '__main__':
+    main()

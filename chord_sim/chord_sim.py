91f1a7fb381e1ce093fc1b00914dbdd5bfadfe48
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-10-30 20:58:43 +0900

    change paramater: changed finger table construction batch execution times from 2 to 1.

--- chord_sim/chord_sim.py
+++ chord_sim/chord_sim.py
@@ -860,7 +860,7 @@ def node_join_and_stabilize_th():
         # を実行する
         # 実際の運用でもネットワークが安定した状態で後続のノードが入っているというのが通常なので（広域のシステムだと）、
         # それとは整合する処理の流れだと思われる
-        for n in range(101):
+        for n in range(101): # stabilize_successor batch 100times, stabilize_finger_table batch 2times
             # ループのうち、最初の一定回数は stabilize_successorが走り、残りはstabilize_finger_tableが走るように
             # 実装されている
             do_stabilize_once_at_all_node()

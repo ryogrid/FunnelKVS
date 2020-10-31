031c79680811e376ffab67b6700d47ecf558b0cc
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-10-31 14:51:32 +0900

    excution times of stabilize successor operation batch is changed from 100 to 1000.

--- chord_sim/chord_sim.py
+++ chord_sim/chord_sim.py
@@ -671,14 +671,14 @@ def do_stabilize_once_at_all_node():
 
     # TODO: 実システムではあり得ないが、stabilize_successor と stabilize_finger_table
     #       が同じChordネットワーク初期化後の同じ時間帯に動作しないようにしてある
-    if done_stabilize_successor_cnt < 100:
+    if done_stabilize_successor_cnt < 1000:
         for node in all_node_dict.values():
           node.stabilize_successor()
           done_stabilize_successor_cnt += 1
           ChordUtil.dprint("do_stabilize_on_random_node__successor," + str(node.node_info.born_id) + ","
                            + hex(node.node_info.node_id) + "," + ChordUtil.conv_id_to_ratio_str(node.node_info.node_id) + ","
                            + str(done_stabilize_successor_cnt))
-    elif done_stabilize_successor_cnt == 100:
+    elif done_stabilize_successor_cnt == 1000:
         check_nodes_connectivity()
         done_stabilize_successor_cnt += 1 # check_nodes_connectivity が複数回実行されないようにするため
         # is_stabiize_finished = True
@@ -686,7 +686,7 @@ def do_stabilize_once_at_all_node():
     # ネットワーク上のノードにおいて、successorの情報が適切に設定された状態とならないと、
     # stabilize_finger_talbleはほどんと意味を成さずに終了してしまう場合が多いと思われるため
     # successor（とpredecessor）に関するstabilizeが十分に成されるまで待つ
-    if done_stabilize_successor_cnt > 100:
+    if done_stabilize_successor_cnt > 1000:
         # 全てのノードについて処理を行う
 
         # テーブルの下から順に全て更新する

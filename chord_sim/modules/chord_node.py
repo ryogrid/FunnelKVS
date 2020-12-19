a2bc2cc69e971101888cbe6845fe51b8bab2480a
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-12-19 19:58:31 +0900

    modified around constructing chain codes. chain health check becomes NG with CPython (PyPy is not tried yet).

--- chord_sim/modules/chord_node.py
+++ chord_sim/modules/chord_node.py
@@ -34,14 +34,17 @@ class ChordNode:
         if first_node:
             # 最初の1ノードの場合
 
-            # successorは自身として終了する
+            # successorとpredecessorは自身として終了する
             self.node_info.successor_info_list.append(self.node_info)
+            self.node_info.predecessor_info = self.node_info
+
             # 最初の1ノードなので、joinメソッド内で行われるsuccessor からの
             # データの委譲は必要ない
 
-            # joinの処理の中でsuccessorをfinger_tableのインデックス0に設定する
-            # が、first nodeは stabilize_successorでsuccessorを張り替える
-            # 際にその処理を行う
+            # # joinの処理の中でsuccessorをfinger_tableのインデックス0に設定する
+            # # が、first nodeは stabilize_successorでsuccessorを張り替える
+            # # 際にその処理を行う
+
             return
         else:
             self.join(node_address)
@@ -64,17 +67,32 @@ class ChordNode:
         # self.node_info.finger_table[0] = self.node_info.successor_info_list[0].get_partial_deepcopy()
         self.node_info.finger_table[0] = ChordUtil.get_deepcopy_of_successor_list(self.node_info.successor_info_list)
 
-        # successorから見たpredecessorおよび、自身から見たpredecessorの情報は
-        # このタイミングで更新可能なはずなのでここで一度stabilize_successorを呼び出してしまう
-        self.stabilize_successor()
+        if tyukai_node.node_info.node_id == tyukai_node.node_info.successor_info_list[0].node_id:
+            # secondノードの場合の考慮 (仲介ノードは必ずfirst node)
 
-        # 上記のstabilize_successorの呼び出しにより、successorが元々 predecessor の情報を保持
-        # していた場合は、自身にそのノードが predecessor として設定されているはず
-        # そして、その場合、自身の predecessor には自身を successorとして認識してもらわないと困る
-        # のでそこの確認処理を行わせる
-        if self.node_info.predecessor_info != None:
-            predecessor_node = ChordUtil.get_node_by_address(cast(NodeInfo, self.node_info.predecessor_info).address_str)
-            predecessor_node.stabilize_successor()
+            # 2ノードでsuccessorでもpredecessorでも、チェーン構造で正しい環が構成されるよう強制的に全て設定してしまう
+            self.node_info.predecessor_info = tyukai_node.node_info.get_partial_deepcopy()
+            tyukai_node.node_info.predecessor_info = self.node_info.get_partial_deepcopy()
+            tyukai_node.node_info.successor_info_list[0] = self.node_info.get_partial_deepcopy()
+            # fingerテーブルの0番エントリも強制的に設定する
+            tyukai_node.node_info.finger_table[0] = [self.node_info.get_partial_deepcopy()]
+        else:
+            # # successorから見たpredecessorおよび、自身から見たpredecessorの情報は
+            # # このタイミングで更新可能なはずなのでここで一度stabilize_successorを呼び出してしまう
+            # self.stabilize_successor()
+            #
+            # # 上記のstabilize_successorの呼び出しにより、successorが元々 predecessor の情報を保持
+            # # していた場合は、自身にそのノードが predecessor として設定されているはず
+            # # そして、その場合、自身の predecessor には自身を successorとして認識してもらわないと困る
+            # # のでそこの確認処理を行わせる
+            # if self.node_info.predecessor_info != None:
+            #     predecessor_node = ChordUtil.get_node_by_address(cast(NodeInfo, self.node_info.predecessor_info).address_str)
+            #     predecessor_node.stabilize_successor()
+
+            # 強制的に自身を既存のチェーンに挿入する
+            # successorは predecessorの 情報を必ず持っていることを前提とする
+            self.node_info.predecessor_info = cast(NodeInfo, successor.node_info.predecessor_info).get_partial_deepcopy()
+            successor.node_info.predecessor_info = self.node_info.get_partial_deepcopy()
 
         # 自ノードの情報、仲介ノードの情報、successorとして設定したノードの情報
         ChordUtil.dprint("join," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
@@ -264,15 +282,15 @@ class ChordNode:
     # id が自身の正しい predecessor でないかチェックし、そうであった場合、経路表の情報を更新する
     # 本メソッドはstabilize処理の中で用いられる
     def check_predecessor(self, id : int, node_info : NodeInfo):
-        if self.node_info.predecessor_info == None:
-            # 未設定状態なので確認するまでもなく、predecessorらしいと判断し
-            # 経路情報に設定し、処理を終了する
-            self.node_info.predecessor_info = node_info.get_partial_deepcopy()
-            ChordUtil.dprint("check_predecessor_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
-                  + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
-                  + ChordUtil.gen_debug_str_of_node(self.node_info.predecessor_info))
-
-            return
+        # if self.node_info.predecessor_info == None:
+        #     # 未設定状態なので確認するまでもなく、predecessorらしいと判断し
+        #     # 経路情報に設定し、処理を終了する
+        #     self.node_info.predecessor_info = node_info.get_partial_deepcopy()
+        #     ChordUtil.dprint("check_predecessor_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+        #           + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
+        #           + ChordUtil.gen_debug_str_of_node(self.node_info.predecessor_info))
+        #
+        #     return
 
         ChordUtil.dprint("check_predecessor_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
               + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
@@ -292,33 +310,29 @@ class ChordNode:
     # successorおよびpredicessorに関するstabilize処理を行う
     # predecessorはこの呼び出しで初めて設定される
     def stabilize_successor(self):
-        ChordUtil.dprint("stabilize_successor_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+        ChordUtil.dprint("stabilize_successor_0," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
               + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
 
-        # firstノードに対する考慮（ノード作成時に自身をsuccesorに設定しているために自身だけ
-        # でsuccessorチェーンのループを作ったままになってしまうことを回避する）
-        if self.node_info.predecessor_info != None and (self.node_info.node_id == self.node_info.successor_info_list[0].node_id):
-            ChordUtil.dprint("stabilize_successor_1_5," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
-                             + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
-            # secondノードがjoin済みであった場合、当該ノードのstabilize_successorによって
-            # secondノードがpredecessorとして設定されているはずなので、succesorをそちら
-            # に張り替える
-            if self.node_info.predecessor_info != None:
-                self.node_info.successor_info_list[0] = self.node_info.predecessor_info.get_partial_deepcopy()
-
-                # finger_tableのインデックス0は必ずsuccessorになるはずなので、設定しておく
-                # self.node_info.finger_table[0] = self.node_info.successor_info_list[0].get_partial_deepcopy()
-                self.node_info.finger_table[0] = ChordUtil.get_deepcopy_of_successor_list(
-                    self.node_info.successor_info_list)
-
-                # この修正を入れた時点での実装だと、secondノードがjoinした際、secondノードの predecessor が未設定の
-                # 状態になっているはずであり、それは正常な状態でないため、自身を設定する
-                second_node = ChordUtil.get_node_by_address(self.node_info.predecessor_info.address_str)
-                second_node.node_info.predecessor_info = self.node_info.get_partial_deepcopy()
-                return
-            else:
-                # secondノードがまだjoinしてきていない
-                return
+        # # firstノードに対する考慮（ノード作成時に自身をsuccesorに設定しているために自身だけ
+        # # でsuccessorチェーンのループを作ったままになってしまうことを回避する）
+        # if self.node_info.predecessor_info != None and (self.node_info.node_id == self.node_info.successor_info_list[0].node_id):
+        #     ChordUtil.dprint("stabilize_successor_1_5," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+        #                      + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
+        #     # このルートに入っていることはsecondノードがjoin済みであることを意味する.
+        #     # 当該ノードのstabilize_successorによってsecondノードがpredecessorとして設定されているはずなので、succesorをそちら
+        #     # に張り替える
+        #     self.node_info.successor_info_list[0] = self.node_info.predecessor_info.get_partial_deepcopy()
+        #
+        #     # finger_tableのインデックス0は必ずsuccessorになるはずなので、設定しておく
+        #     # self.node_info.finger_table[0] = self.node_info.successor_info_list[0].get_partial_deepcopy()
+        #     self.node_info.finger_table[0] = ChordUtil.get_deepcopy_of_successor_list(
+        #         self.node_info.successor_info_list)
+        #
+        #     # この修正を入れた時点での実装だと、secondノードがjoinした際、secondノードの predecessor が未設定の
+        #     # 状態になっているはずであり、それは正常な状態でないため、自身を設定する
+        #     second_node = ChordUtil.get_node_by_address(self.node_info.predecessor_info.address_str)
+        #     second_node.node_info.predecessor_info = self.node_info.get_partial_deepcopy()
+        #     return
 
         # 自身のsuccessorに、当該ノードが認識しているpredecessorを訪ねる
         # 自身が保持している successor_infoのミュータブルなフィールドは最新の情報でない
@@ -326,15 +340,15 @@ class ChordNode:
         # の参照を得る
         successor = ChordUtil.get_node_by_address(self.node_info.successor_info_list[0].address_str)
         successor_info = successor.node_info
-        # successor_info = self.node_info.successor_info
-        if successor_info.predecessor_info == None:
-            # successor が predecessor を未設定であった場合は自身を predecessor として保持させて
-            # 処理を終了する
-            successor_info.predecessor_info = self.node_info.get_partial_deepcopy()
 
-            ChordUtil.dprint("stabilize_successor_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
-                             + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
-            return
+        # if successor_info.predecessor_info == None:
+        #     # successor が predecessor を未設定であった場合は自身を predecessor として保持させて
+        #     # 処理を終了する
+        #     successor_info.predecessor_info = self.node_info.get_partial_deepcopy()
+        #
+        #     ChordUtil.dprint("stabilize_successor_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+        #                      + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
+        #     return
 
         ChordUtil.dprint("stabilize_successor_3," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
                          + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))

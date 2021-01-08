84f6be98af8af456753a49fa5c1c3d5afd0b8769
Ryo Kanbayashi <ryo.contact@gmail.com>
2021-01-08 17:05:39 +0900

    added debug prints for testing extended stabilize_successor method.

--- chord_sim/modules/chord_node.py
+++ chord_sim/modules/chord_node.py
@@ -300,6 +300,8 @@ class ChordNode:
     def stabilize_successor_inner(self) -> NodeInfo:
         # 本メソッド呼び出しでsuccessorとして扱うノードはsuccessorListの先頭から生きているもの
         # をサーチし、発見したノードとする.
+        ChordUtil.dprint("stabilize_successor_inner_0," + ChordUtil.gen_debug_str_of_node(self.node_info))
+
         successor : ChordNode
         successor_tmp : Optional[ChordNode] = None
         for idx in range(len(self.node_info.successor_info_list)):
@@ -313,12 +315,17 @@ class ChordNode:
             # 起きてはいけない状況なので例外を投げてプログラムを終了させる
             raise Exception("Maybe some parameters related to fault-tolerance of Chord network are not appropriate")
 
-        ChordUtil.dprint("stabilize_successor_first_one_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+        # 生存が確認されたノードを successor[0] として設定する
+        self.node_info.successor_info_list[0] = successor.node_info.get_partial_deepcopy()
+
+        ChordUtil.dprint("stabilize_successor_inner_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
                          + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]))
 
         pred_id_of_successor = cast(NodeInfo, successor.node_info.predecessor_info).node_id
 
-        ChordUtil.dprint("stabilize_successor_first_one_1_5," + hex(pred_id_of_successor))
+        ChordUtil.dprint("stabilize_successor_inner_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                         + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
+                         + str(pred_id_of_successor))
 
         # successorに認識している predecessor の情報をチェックさせて、適切なものに変更させたり、把握していない
         # 自身のsuccessor[0]になるべきノードの存在が判明した場合は 自身の successor[0]をそちらに張り替える.
@@ -326,8 +333,10 @@ class ChordNode:
         # https://www.slideshare.net/did2/chorddht
         if(pred_id_of_successor == self.node_info.node_id):
             # パターン1
-            # 特に訂正は不要なので処理を終了する
-            return successor.node_info.get_partial_deepcopy()
+            # 特に訂正は不要
+            ChordUtil.dprint("stabilize_successor_inner_3," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                             + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
+                             + str(pred_id_of_successor))
         else:
             # 以下、パターン2およびパターン3に対応する処理
 
@@ -353,11 +362,9 @@ class ChordNode:
                     # ば情報を更新してもらう
                     new_successor.check_predecessor(self.node_info.node_id, self.node_info)
 
-                    ChordUtil.dprint("stabilize_successor_first_one_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                    ChordUtil.dprint("stabilize_successor_inner_4," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
                                      + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
                                      + ChordUtil.gen_debug_str_of_node(new_successor.node_info))
-
-                    return new_successor.node_info.get_partial_deepcopy()
                 except NodeIsDownedExectiopn:
                     # 例外発生時は張り替えを中止する
                     #   - successorは変更しない
@@ -365,10 +372,11 @@ class ChordNode:
 
                     # successor[0]の変更は行わず、ダウンしていたノードではなく自身をpredecessorとするよう(間接的に)要請する
                     successor.check_predecessor(self.node_info.node_id, self.node_info)
-                    return successor.node_info.get_partial_deepcopy()
-            else:
-                # successor[0] の変更は不要であった
-                return successor.node_info.get_partial_deepcopy()
+                    ChordUtil.dprint("stabilize_successor_inner_5," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                                     + ChordUtil.gen_debug_str_of_node(self.node_info.successor_info_list[0]) + ","
+                                     + str(cast(NodeInfo, self.node_info.successor_info_list[0].predecessor_info).node_id))
+
+        return self.node_info.successor_info_list[0].get_partial_deepcopy()
 
 
     # successorListに関するstabilize処理を行う
@@ -394,16 +402,23 @@ class ChordNode:
 
         while len(self.node_info.successor_info_list) == gval.SUCCESSOR_LIST_NORMAL_LEN:
             cur_node_info : NodeInfo = cur_node.stabilize_successor_inner()
+            ChordUtil.dprint("stabilize_successor_1," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                             + ChordUtil.gen_debug_str_of_node(cur_node_info))
             if cur_node_info.node_id == self.node_info.node_id:
                 # Chordネットワークに (downしていない状態で) 存在するノード数が gval.SUCCESSOR_LIST_NORMAL_LEN
                 # より多くない場合 successorをたどっていった結果、自ノードにたどり着いてしまうため、その場合は規定の
                 # ノード数を満たしていないが、successor_info_list の更新処理は終了する
+                ChordUtil.dprint("stabilize_successor_2," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                                 + ChordUtil.gen_debug_str_of_node(cur_node_info))
                 break
             updated_list.append(cur_node_info)
             # この呼び出しで例外は発生しない
             cur_node = ChordUtil.get_node_by_address(cur_node_info)
 
         self.node_info.successor_info_list = updated_list
+
+        ChordUtil.dprint("stabilize_successor_3," + ChordUtil.gen_debug_str_of_node(self.node_info) + ","
+                     + str(self.node_info.successor_info_list))
 
     # FingerTableに関するstabilize処理を行う
     # 一回の呼び出しで1エントリを更新する

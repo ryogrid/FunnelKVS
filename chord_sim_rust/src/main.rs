ca4c6eee6148455a608fcd4bde631f931e09cde0
Ryo Kanbayashi <ryo.contact@gmail.com>
2021-06-20 16:19:30 +0900

    wip: writing main func. chord_node::new_and_join func paniced at runtime...

--- chord_sim_rust/src/main.rs
+++ chord_sim_rust/src/main.rs
@@ -743,7 +743,6 @@ fn ftable_mod_and_search_th(){
     }
 }
 
-
 fn example_th() {
     loop{
         let kv_arc_at_heap : Box<ArRmRs<chord_util::KeyValue>>;
@@ -767,7 +766,7 @@ fn example_th() {
     }
 }
 
-fn main() {
+fn main_old() {
 /////// 要素もそれ自身もmutableなグローバルアクセス可能な形で定義されたコレクションの操作の例 //////
     // Vecを操作している処理のブロック
     {
@@ -880,3 +879,310 @@ fn main() {
 
     println!("Hello, world!");
 }
+
+
+/*
+# stabilize_successorの呼び出しが一通り終わったら確認するのに利用する
+# ランダムに選択したノードからsuccessor方向にsuccessorの繋がりでノードを辿って
+# 行って各ノードの情報を出力する
+# また、predecessorの方向にpredecesorの繋がりでもたどって出力する
+def check_nodes_connectivity():
+    ChordUtil.dprint("check_nodes_connectivity_1")
+    print("flush", flush=True)
+    counter : int = 0
+    # まずはsuccessor方向に辿る
+    cur_node_info : NodeInfo = get_a_random_node().node_info
+    start_node_info : NodeInfo = cur_node_info
+    # ノードの総数（is_aliveフィールドがFalseのものは除外して算出）
+    with gval.lock_of_all_node_dict:
+        all_node_num = len(list(filter(lambda node: node.is_alive == True ,list(gval.all_node_dict.values()))))
+    ChordUtil.print_no_lf("check_nodes_connectivity__succ,all_node_num=" + str(all_node_num) + ",already_born_node_num=" + str(gval.already_born_node_num))
+    print(",", flush=True, end="")
+
+    while counter < all_node_num:
+        ChordUtil.print_no_lf(str(cur_node_info.born_id) + "," + ChordUtil.conv_id_to_ratio_str(cur_node_info.node_id) + " -> ")
+
+        # 各ノードはsuccessorの情報を保持しているが、successorのsuccessorは保持しないようになって
+        # いるため、単純にsuccessorのチェーンを辿ることはできないため、各ノードから最新の情報を
+        # 得ることに対応する形とする
+
+        ret = ChordUtil.get_node_by_address(cur_node_info.address_str)
+        if (ret.is_ok):
+            cur_node_info : 'NodeInfo' = cast('ChordNode', ret.result).node_info.successor_info_list[0]
+        else:  # ret.err_code == ErrorCode.InternalControlFlowException_CODE || ret.err_code == ErrorCode.NodeIsDownedException_CODE
+            if cast(int, ret.err_code) == ErrorCode.NodeIsDownedException_CODE:
+                print("")
+                ChordUtil.dprint("check_nodes_connectivity__succ,NODE_IS_DOWNED")
+                return
+            else: #cast(int, ret.err_code) == ErrorCode.InternalControlFlowException_CODE
+                # join中のノードのノードオブジェクトを get_node_by_address しようとした場合に
+                # TargetNodeDoesNotExistExceptionがraiseされてくるのでその場合は、対象ノードのstabilize_successorはあきらめる
+                print("")
+                ChordUtil.dprint("check_nodes_connectivity__succ,TARGET_NODE_DOES_NOT_EXIST_EXCEPTION_IS_RAISED")
+                return
+
+        if cur_node_info == None:
+            print("", flush=True, end="")
+            raise Exception("no successor having node was detected!")
+        counter += 1
+    print("")
+
+    # 2ノード目が参加して以降をチェック対象とする
+    # successorを辿って最初のノードに戻ってきているはずだが、そうなっていない場合は successorの
+    # チェーン構造が正しく構成されていないことを意味するためエラーとして終了する
+    if all_node_num >=2 and cur_node_info.node_id != start_node_info.node_id:
+        ChordUtil.dprint("check_nodes_connectivity_succ_err,chain does not includes all node. all_node_num = "
+                         + str(all_node_num) + ","
+                         + ChordUtil.gen_debug_str_of_node(start_node_info) + ","
+                         + ChordUtil.gen_debug_str_of_node(cur_node_info))
+        # raise exception("SUCCESSOR_CHAIN_IS_NOT_CONSTRUCTED_COLLECTLY")
+    else:
+        ChordUtil.dprint("check_nodes_connectivity_succ_success,chain includes all node. all_node_num = "
+                         + str(all_node_num) + ","
+                         + ChordUtil.gen_debug_str_of_node(start_node_info) + ","
+                         + ChordUtil.gen_debug_str_of_node(cur_node_info))
+
+    # 続いてpredecessor方向に辿る
+    counter = 0
+    cur_node_info = get_a_random_node().node_info
+    start_node_info = cur_node_info
+    ChordUtil.print_no_lf("check_nodes_connectivity__pred,all_node_num=" + str(all_node_num) + ",already_born_node_num=" + str(gval.already_born_node_num))
+    print(",", flush=True, end="")
+    while counter < all_node_num:
+        ChordUtil.print_no_lf(str(cur_node_info.born_id) + "," + ChordUtil.conv_id_to_ratio_str(cur_node_info.node_id) + " -> ")
+        ret = ChordUtil.get_node_by_address(cur_node_info.address_str)
+        if (ret.is_ok):
+            cur_node_info: 'ChordNode' = cast('ChordNode', ret.result).node_info.predecessor_info
+        else:  # ret.err_code == ErrorCode.InternalControlFlowException_CODE || ret.err_code == ErrorCode.NodeIsDownedException_CODE
+            if cast(int, ret.err_code) == ErrorCode.NodeIsDownedException_CODE:
+                print("")
+                ChordUtil.dprint("check_nodes_connectivity__pred,NODE_IS_DOWNED")
+                return
+            else: #cast(int, ret.err_code) == ErrorCode.InternalControlFlowException_CODE
+                # join中のノードのノードオブジェクトを get_node_by_address しようとした場合に
+                # TargetNodeDoesNotExistExceptionがraiseされてくるのでその場合は、対象ノードのstabilize_successorはあきらめる
+                print("")
+                ChordUtil.dprint("check_nodes_connectivity__pred,TARGET_NODE_DOES_NOT_EXIST_EXCEPTION_IS_RAISED")
+                return
+
+        if cur_node_info == None:
+            # 先を追っていけないのでチェックを終了する
+            ChordUtil.dprint("check_nodes_connectivity__pred,PREDECESSOR_INFO_IS_NONE")
+            return
+
+        counter += 1
+
+    print("")
+
+    # 2ノード目から本来チェック可能であるべきだが、stabilize処理の実行タイミングの都合で
+    # 2ノード目がjoinした後、いくらかpredecessorがNoneの状態が生じ、そのタイミングで本チェックが走る場合が
+    # あり得るため、余裕を持たせて5ノード目以降からチェックする
+    # successorを辿って最初のノードに戻ってきているはずだが、そうなっていない場合は successorの
+    # チェーン構造が正しく構成されていないことを意味するためエラーとして終了する
+    if all_node_num >=5 and cur_node_info.node_id != start_node_info.node_id:
+        ChordUtil.dprint("check_nodes_connectivity_pred_err,chain does not includes all node. all_node_num = "
+                         + str(all_node_num) + ","
+                         + ChordUtil.gen_debug_str_of_node(start_node_info) + ","
+                         + ChordUtil.gen_debug_str_of_node(cur_node_info))
+        # raise Exception("PREDECESSOR_CHAIN_IS_NOT_CONSTRUCTED_COLLECTLY")
+    else:
+        ChordUtil.dprint("check_nodes_connectivity_pred_success,chain includes all node. all_node_num = "
+                         + str(all_node_num) + ","
+                         + ChordUtil.gen_debug_str_of_node(start_node_info) + ","
+                         + ChordUtil.gen_debug_str_of_node(cur_node_info))
+*/
+
+/*
+# ランダムに仲介ノードを選択し、そのノードに仲介してもらう形でネットワークに参加させる
+def add_new_node():
+    # # ロックの取得
+    # gval.lock_of_all_data.acquire()
+
+    if Stabilizer.need_join_retry_node != None:
+        # 前回の呼び出しが失敗していた場合はリトライを行う
+        tyukai_node = cast('ChordNode', Stabilizer.need_join_retry_tyukai_node)
+        new_node = cast('ChordNode', Stabilizer.need_join_retry_node)
+        new_node.stabilizer.join(tyukai_node.node_info.address_str)
+        if Stabilizer.need_join_retry_node == None:
+            # リトライ情報が再設定されていないためリトライに成功したと判断
+            ChordUtil.dprint(
+                "add_new_node_1,retry of join is succeeded," + ChordUtil.gen_debug_str_of_node(new_node.node_info))
+        else:
+            ChordUtil.dprint(
+                "add_new_node_2,retry of join is failed," + ChordUtil.gen_debug_str_of_node(new_node.node_info))
+    else:
+        tyukai_node = get_a_random_node()
+        new_node = ChordNode(tyukai_node.node_info.address_str)
+
+    if Stabilizer.need_join_retry_node == None:
+        # join処理(リトライ時以外はChordNodeクラスのコンストラクタ内で行われる)が成功していれば
+        gval.all_node_dict[new_node.node_info.address_str] = new_node
+        # join処理のうち、ネットワーク参加時に必ずしも完了していなくてもデータの整合性やネットワークの安定性に
+        # に問題を生じさせないような処理をここで行う（当該処理がノード内のタスクキューに入っているのでそれを実行する形にする）
+        new_node.tqueue.exec_first()
+
+    # # ロックの解放
+    # gval.lock_of_all_data.release()
+*/
+
+/*
+def do_stabilize_successor_th(node_list : List[ChordNode]):
+    for times in range(0, gval.STABILIZE_SUCCESSOR_BATCH_TIMES):
+        for node in node_list:
+            # try:
+                #node.stabilizer.stabilize_successor()
+            ret = node.stabilizer.stabilize_successor()
+            if (ret.is_ok):
+                pass
+            else:  # ret.err_code == ErrorCode.InternalControlFlowException_CODE
+                # join中のノードのノードオブジェクトを get_node_by_address しようとした場合に
+                # InternalCtronlFlowExceptionがraiseされてくるのでその場合は、対象ノードのstabilize_finger_tableはあきらめる
+                ChordUtil.dprint(
+                    "do_stabilize_successor_th," + ChordUtil.gen_debug_str_of_node(node.node_info)
+                    + ",STABILIZE_FAILED_DUE_TO_INTERNAL_CONTROL_FLOW_EXCEPTION_RAISED")
+*/
+
+/*
+def do_stabilize_ftable_th(node_list : List[ChordNode]):
+    for times in range(0, gval.STABILIZE_FTABLE_BATCH_TIMES):
+        for table_idx in range(0, gval.ID_SPACE_BITS):
+            for node in node_list:
+                ret = node.stabilizer.stabilize_finger_table(table_idx)
+                if (ret.is_ok):
+                    pass
+                else:  # ret.err_code == ErrorCode.InternalControlFlowException_CODE
+                    # join中のノードのノードオブジェクトを get_node_by_address しようとした場合に
+                    # InternalCtronlFlowExceptionがraiseされてくるのでその場合は、対象ノードのstabilize_finger_tableはあきらめる
+                    ChordUtil.dprint(
+                        "do_stabilize_ftable_th," + ChordUtil.gen_debug_str_of_node(node.node_info)
+                        + ",STABILIZE_FAILED_DUE_TO_INTERNAL_CONTROL_FLOW_EXCEPTION_RAISED")
+*/
+
+/*
+def do_stabilize_onace_at_all_node_ftable(node_list : List[ChordNode]) -> List[Thread]:
+    list_len = len(node_list)
+    range_start = 0
+    # 小数点以下切り捨て
+    basic_pass_node_cnt = int(list_len / gval.STABILIZE_THREAD_NUM)
+    thread_list : List[Thread] = []
+    for thread_idx in range(0, gval.STABILIZE_THREAD_NUM):
+        if thread_idx == gval.STABILIZE_THREAD_NUM - 1:
+            thread = threading.Thread(target=do_stabilize_ftable_th, name="ftable-" + str(thread_idx),
+                                      args=([node_list[range_start:-1]]))
+        else:
+            thread = threading.Thread(target=do_stabilize_successor_th, name="ftable-" + str(thread_idx),
+                                      args=([node_list[range_start:range_start + basic_pass_node_cnt]]))
+            range_start += basic_pass_node_cnt
+        thread.start()
+        thread_list.append(thread)
+
+    return thread_list
+*/
+
+/*
+# all_node_id辞書のvaluesリスト内から重複なく選択したノードに stabilize のアクションをとらせていく
+def do_stabilize_once_at_all_node():
+    ChordUtil.dprint("do_stabilize_once_at_all_node_0,START")
+    with gval.lock_of_all_node_dict:
+        node_list = list(gval.all_node_dict.values())
+        shuffled_node_list : List[ChordNode] = random.sample(node_list, len(node_list))
+    thread_list_succ : List[Thread] = do_stabilize_onace_at_all_node_successor(shuffled_node_list)
+    thread_list_ftable : List[Thread] = do_stabilize_onace_at_all_node_ftable(shuffled_node_list)
+
+    # 全てのスレッドが終了するまで待つ
+    # 一つの呼び出しごとにブロックするが、その間に別にスレッドが終了しても
+    # スレッドの処理が終了していることは担保できるため問題ない
+    for thread in thread_list_succ:
+        thread.join()
+    for thread in thread_list_ftable:
+        thread.join()
+
+    check_nodes_connectivity()
+*/
+
+/*
+# TODO: 対応する処理を行うスクリプトの類が必要 node_join_th
+def node_join_th():
+    while gval.already_born_node_num < gval.NODE_NUM_MAX:
+        if gval.already_born_node_num == gval.KEEP_NODE_NUM:
+            time.sleep(60.0)
+            gval.is_network_constructed = True
+            gval.JOIN_INTERVAL_SEC = 120.0 #20.0
+            # # TODO: デバッグのために1000ノードに達したらjoinを止める。後で元に戻すこと!
+            # #       at node_join_th
+            # break
+
+        add_new_node()
+        time.sleep(gval.JOIN_INTERVAL_SEC)
+*/
+
+/*        
+def stabilize_th():
+    while True:
+        # 内部で適宜ロックを解放することで他のスレッドの処理も行えるようにしつつ
+        # 呼び出し時点でのノードリストを対象に stabilize 処理を行う
+        do_stabilize_once_at_all_node()
+*/
+fn main() {
+    // 最初の1ノードはここで登録する
+    let first_node = chord_node::new_and_join("".to_string(), false);
+    let first_node_refcell = get_refcell_from_arc_with_locking!(first_node);
+    let first_node_refmut = get_refmut_from_refcell!(first_node_refcell);
+    
+    first_node_refmut.is_join_op_finished.store(true, Ordering::Relaxed);
+/*    
+    gval.all_node_dict[first_node.node_info.address_str] = first_node
+    time.sleep(0.5) #次に生成するノードが同一のアドレス文字列を持つことを避けるため
+
+    node_join_th_handle = threading.Thread(target=node_join_th, daemon=True)
+    node_join_th_handle.start()
+
+    stabilize_th_handle = threading.Thread(target=stabilize_th, daemon=True)
+    stabilize_th_handle.start()
+*/
+
+    // data_put_th_handle = threading.Thread(target=data_put_th, daemon=True)
+    // data_put_th_handle.start()
+
+    // data_get_th_handle = threading.Thread(target=data_get_th, daemon=True)
+    // data_get_th_handle.start()
+
+    // node_kill_th_handle = threading.Thread(target=node_kill_th, daemon=True)
+    // node_kill_th_handle.start()
+
+    println!("Hello, world!");
+/*
+    while True:
+        time.sleep(1)
+*/
+}
+/*    
+    # 再現性のため乱数シードを固定
+    # ただし、複数スレッドが存在し、個々の処理の終了するタイミングや、どのタイミングで
+    # スイッチするかは実行毎に異なる可能性があるため、あまり意味はないかもしれない
+    random.seed(1337)
+
+    # 最初の1ノードはここで登録する
+    first_node = ChordNode("THIS_VALUE_IS_NOT_USED", first_node=True)
+    first_node.is_join_op_finished = True
+    gval.all_node_dict[first_node.node_info.address_str] = first_node
+    time.sleep(0.5) #次に生成するノードが同一のアドレス文字列を持つことを避けるため
+
+    node_join_th_handle = threading.Thread(target=node_join_th, daemon=True)
+    node_join_th_handle.start()
+
+    stabilize_th_handle = threading.Thread(target=stabilize_th, daemon=True)
+    stabilize_th_handle.start()
+
+    data_put_th_handle = threading.Thread(target=data_put_th, daemon=True)
+    data_put_th_handle.start()
+
+    data_get_th_handle = threading.Thread(target=data_get_th, daemon=True)
+    data_get_th_handle.start()
+
+    node_kill_th_handle = threading.Thread(target=node_kill_th, daemon=True)
+    node_kill_th_handle.start()
+
+    while True:
+        time.sleep(1)
+*/

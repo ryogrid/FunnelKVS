a09e022911ba3701e25f13e9f57531e201f144e5
Ryo Kanbayashi <ryo.contact@gmail.com>
2021-11-12 10:50:30 +0900

    wip: global put and get worked over gRPC except first node.

--- tools/rust_dkvs.bat
+++ tools/rust_dkvs.bat
@@ -1,4 +1,4 @@
 @echo off
-@rem..\target\debug\rust_dkvs.exe %1 %2 %3 %4 %5 %6 > stdout%1.txt 2>&1
-..\target\release\rust_dkvs.exe %1 %2 %3 %4 %5 %6 > stdout%1.txt 2>&1
+..\target\debug\rust_dkvs.exe %1 %2 %3 %4 %5 %6 > stdout%1.txt 2>&1
+@rem..\target\release\rust_dkvs.exe %1 %2 %3 %4 %5 %6 > stdout%1.txt 2>&1
 
